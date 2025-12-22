6dba55221e4305b9941de5402141f21bc39f1c4e
Behrooz Shahin <1695223@bdeb.qc.ca>
2025-12-22 14:14:01 -0500

    repository tested

--- src/main/java/com/example/inventory/InventoryApplication.java
+++ src/main/java/com/example/inventory/InventoryApplication.java
@@ -2,8 +2,12 @@ package com.example.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.inventory.model")  // Ensure entities are scanned
+@EnableJpaRepositories(basePackages = "com.example.inventory.repository")
 public class InventoryApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/inventory/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/inventory/repository/ProductRepositoryTest.java
@@ -1,17 +1,34 @@
 package com.example.inventory.repository;
 
 import com.example.inventory.model.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DataJpaTest
-@TestPropertySource(locations = "classpath:application-test.properties")
 class ProductRepositoryTest {
+    @Autowired
+    ProductRepository repository;
+
+    static Product product;
+
+    @BeforeAll
+    static void setUp() {
+        product = new Product("Laptop", "Dell Inspiron", 1090.12, 50);
+    }
 
     @Test
     void testSaveProduct() {
-        Product product = new Product("Laptop","Dell inspiron",1090.12,50);
-
+        Product saved = repository.save(product);
+        Optional<Product> optionalProduct = repository.findById(saved.getId());
+        assertFalse(optionalProduct.isEmpty());
+        assertEquals(optionalProduct.get().getName(),product.getName());
     }
 }
